d1cb49b3355510d198ed9213e7e64e87f2ec3732
Augusto Marques <markesaf16@gmail.com>
2023-08-16 16:30:31 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 import java.util.Date;
+import model.dao.SellerDao;
 
 public class Program {
     public static void main(String[]args){
@@ -10,6 +12,8 @@ public class Program {
 
         Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(seller);
+
+        SellerDao sellerDao = DaoFactory.CreateSellerDao();
 
         System.out.println(obj);
     }
