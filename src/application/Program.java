c361b3ffd123351a19541c773da8cbacd2747ecf
Augusto Marques <markesaf16@gmail.com>
2023-08-18 00:14:43 -0300

    Reusing Instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,9 +9,9 @@ import model.dao.SellerDao;
 public class Program {
     public static void main(String[]args){
 
-
-
         SellerDao sellerDao = DaoFactory.CreateSellerDao();
+
+        System.out.println("===== TEST 1: seller findById =====");
         Seller seller =sellerDao.findById(3);
 
         System.out.println(seller);
